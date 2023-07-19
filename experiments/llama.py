24b02259f168495b1701b5c3483964eaa36d8496
Damith Premasiri <damithpremasiri@gmail.com>
2023-07-19 01:27:58 +0100

    llama prompt

--- experiments/llama.py
+++ experiments/llama.py
@@ -1,18 +1,38 @@
-from transformers import AutoTokenizer, LlamaForCausalLM
-from transformers import pipeline
+import pandas as pd
+import torch
+from langchain import HuggingFacePipeline
+from langchain import PromptTemplate, LLMChain
+from sklearn import metrics
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
 
-pipe = pipeline("text-generation", model="meta-llama/Llama-2-7b-hf")
+model = "meta-llama/Llama-2-7b-hf"
 
-model = LlamaForCausalLM.from_pretrained('meta-llama/Llama-2-7b-hf')
-tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-2-7b-hf')
+tokenizer = AutoTokenizer.from_pretrained(model)
 
+pipeline = pipeline(
+    "text-generation",  # task
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+    max_memory={2: "20GIB", 3: "20GIB"},
+    max_length=200,
+    do_sample=True,
+    top_k=1,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id
+)
+
+llm = HuggingFacePipeline(pipeline=pipeline, model_kwargs={'temperature': 0})
+template = """
+Question: {question}
+Answer:"""
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
 
 while True:
-    prompt = input("User : ")
-
-    inputs = tokenizer(prompt, return_tensors="pt")
-
-    # Generate
-    generate_ids = model.generate(inputs.input_ids, max_length=30)
-    output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-    print(f'System : {output}')
+    question = input("User Input : ")
+    response = llm_chain.run(question)
+    print(response)
